e800565d8385e0d9decb185967ea6dc21e5bda29
Ruslan <ruslanzel@list.ru>
2022-09-03 11:15:13 +0300

    Сравнить бины по ссылке.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanCopy = (HelloWorld) applicationContext.getBean("helloworld");
+
+//        System.out.println(bean.getMessage());
+//        System.out.println(beanCopy.getMessage());
+        System.out.println(bean == beanCopy);
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catCopy = (Cat) applicationContext.getBean("cat");
+
+//        System.out.println(cat.getMessageCat());
+//        System.out.println(catCopy.getMessageCat());
+        System.out.println(cat == catCopy);
     }
 }
